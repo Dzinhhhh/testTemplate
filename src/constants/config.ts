61dd7e965b5869d1dc6fb0079d1a3b69672c9a1b
Vinh Dang <111186679+Dzinhhhh@users.noreply.github.com>
2026-02-01 23:48:07 +0700

    Update config with new name and title

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,12 +40,12 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
+    title: "Dzinh — 3D Portfolio",
+    fullName: "Dzinh",
     email: "johndoe@mail.com",
   },
   hero: {
-    name: "John Doe",
+    name: "Dzinh",
     p: ["I develop 3D visuals, user", "interfaces and web applications"],
   },
   contact: {
